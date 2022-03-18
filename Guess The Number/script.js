f45f39d4b2787f1cdeb428bae1f3391be54de9c9
Udith Biju <udithbiju2@gmail.com>
2022-03-19 01:05:45 +0530

    added increment decrement buttons

--- Guess The Number/script.js
+++ Guess The Number/script.js
@@ -3,7 +3,23 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1; // Math.trunc is used for
 let score = 20;
 let highscore = 0;
 
+//increment button
+document.querySelector('.inc').addEventListener('click', function () {
+  let inc = Number(document.querySelector('.guess').value);
+  if(inc<20)inc = inc + 1;
+  document.querySelector('.guess').value = inc;
 
+});
+
+//decrement button
+document.querySelector('.dec').addEventListener('click', function () {
+  let dec = Number(document.querySelector('.guess').value);
+  if (dec > 0) {
+    dec = dec - 1;
+  }
+  document.querySelector('.guess').value = dec;
+
+});
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -67,4 +83,5 @@ document.querySelector('.again').addEventListener('click', function () {
 
   document.querySelector('.guess').value = ' ';
   document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?'
 });
